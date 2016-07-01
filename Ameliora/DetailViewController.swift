5af7b3d8b89e26e2848c308d8274f5363df2cf3e
Alex Ianchici <rawr@alrex.ca>
2016-06-30 23:06:54 -0700

    display the run

--- Ameliora/DetailViewController.swift
+++ Ameliora/DetailViewController.swift
@@ -38,21 +38,90 @@ class DetailViewController: UIViewController {
     configureView()
   }
 
-  func configureView() {
-    let distanceQuantity = HKQuantity(unit: HKUnit.meterUnit(), doubleValue: run.distance.doubleValue)
-    distanceLabel.text = "Distance: " + distanceQuantity.description
+    func configureView() {
+        let distanceQuantity = HKQuantity(unit: HKUnit.meterUnit(), doubleValue: run.distance.doubleValue)
+        distanceLabel.text = "Distance: " + distanceQuantity.description
+
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = .MediumStyle
+        dateLabel.text = dateFormatter.stringFromDate(run.timestamp)
+
+        let secondsQuantity = HKQuantity(unit: HKUnit.secondUnit(), doubleValue: run.duration.doubleValue)
+        timeLabel.text = "Time: " + secondsQuantity.description
+
+        let paceUnit = HKUnit.secondUnit().unitDividedByUnit(HKUnit.meterUnit())
+        let paceQuantity = HKQuantity(unit: paceUnit, doubleValue: run.duration.doubleValue / run.distance.doubleValue)
+        paceLabel.text = "Pace: " + paceQuantity.description
+        loadMap()
+    }
     
-    let dateFormatter = NSDateFormatter()
-    dateFormatter.dateStyle = .MediumStyle
-    dateLabel.text = dateFormatter.stringFromDate(run.timestamp)
+    func mapRegion() -> MKCoordinateRegion {
+        let initialLoc = run.locations.firstObject as! Location
+        
+        var minLat = initialLoc.latitude.doubleValue
+        var minLng = initialLoc.longitude.doubleValue
+        var maxLat = minLat
+        var maxLng = minLng
+        
+        let locations = run.locations.array as! [Location]
+        
+        for location in locations {
+            minLat = min(minLat, location.latitude.doubleValue)
+            minLng = min(minLng, location.longitude.doubleValue)
+            maxLat = max(maxLat, location.latitude.doubleValue)
+            maxLng = max(maxLng, location.longitude.doubleValue)
+        }
+        
+        return MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: (minLat + maxLat)/2,
+                longitude: (minLng + maxLng)/2),
+            span: MKCoordinateSpan(latitudeDelta: (maxLat - minLat)*1.1,
+                longitudeDelta: (maxLng - minLng)*1.1))
+    }
     
-    let secondsQuantity = HKQuantity(unit: HKUnit.secondUnit(), doubleValue: run.duration.doubleValue)
-    timeLabel.text = "Time: " + secondsQuantity.description
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        if !overlay.isKindOfClass(MKPolyline) {
+            return nil
+        }
+        
+        let polyline = overlay as! MKPolyline
+        let renderer = MKPolylineRenderer(polyline: polyline)
+        renderer.strokeColor = UIColor.blackColor()
+        renderer.lineWidth = 3
+        return renderer
+    }
     
-    let paceUnit = HKUnit.secondUnit().unitDividedByUnit(HKUnit.meterUnit())
-    let paceQuantity = HKQuantity(unit: paceUnit, doubleValue: run.duration.doubleValue / run.distance.doubleValue)
-    paceLabel.text = "Pace: " + paceQuantity.description
-  }
+    func polyline() -> MKPolyline {
+        var coords = [CLLocationCoordinate2D]()
+        
+        let locations = run.locations.array as! [Location]
+        for location in locations {
+            coords.append(CLLocationCoordinate2D(latitude: location.latitude.doubleValue,
+                longitude: location.longitude.doubleValue))
+        }
+        
+        return MKPolyline(coordinates: &coords, count: run.locations.count)
+    }
+    
+    func loadMap() {
+        if run.locations.count > 0 {
+            mapView.hidden = false
+            
+            // Set the map bounds
+            mapView.region = mapRegion()
+            
+            // Make the line(s!) on the map
+            mapView.addOverlay(polyline())
+        } else {
+            // No locations were found!
+            mapView.hidden = true
+            
+            UIAlertView(title: "Error",
+                        message: "Sorry, this run has no locations saved",
+                        delegate:nil,
+                        cancelButtonTitle: "OK").show()
+        }
+    }
 }
 
 // MARK: - MKMapViewDelegate
